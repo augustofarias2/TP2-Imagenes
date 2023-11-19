438dea15ecbae985f3921ce367a34f9cfb5fe2b0
augustofarias2 <augustofarias027@gmail.com>
2023-11-19 19:54:03 -0300

    update casi final monedas

--- ej1.py
+++ ej1.py
@@ -7,6 +7,8 @@ img= cv2.imread("monedas.jpg")
 img_color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.imshow(img_color), plt.show(block = False)
 
+#_____________________________________________________________EJERCICIO 1________________________________________________________________________________________________
+#________________________________________________________________________________________________________________________________________________________________________
 #Imagen filtrada gray scale
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 plt.imshow(img_gray, cmap="gray"), plt.show(block = False)
@@ -15,14 +17,12 @@ plt.imshow(img_gray, cmap="gray"), plt.show(block = False)
 img_filtered = cv2.medianBlur(img_gray, 6)
 plt.imshow(img_filtered, cmap = "gray"), plt.show(block=False)
 
-
 #Canny
 img_canny_CV2 = cv2.Canny(img_filtered, 35, 110)
 plt.imshow(img_canny_CV2, cmap="gray"), plt.show(block = False)
 
 #Dilato
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
-img_dilated = cv2.dilate(img_canny_CV2, kernel, iterations=1)
+img_dilated = cv2.dilate(img_canny_CV2, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12)), iterations=1)
 img_dilated = cv2.dilate(img_dilated, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=1)
 plt.imshow(img_dilated, cmap = "gray"), plt.show(block=False)
 
@@ -30,8 +30,7 @@ plt.imshow(img_dilated, cmap = "gray"), plt.show(block=False)
 #Obtener estructura
 img_open = cv2.morphologyEx(img_dilated, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11)))
 img_close = cv2.morphologyEx(img_open, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (16, 16)))
-plt.imshow(img_close, cmap="gray")
-plt.show(block= False)
+plt.imshow(img_close, cmap="gray"), plt.show(block= False)
 
 
 # ---------------------------------------------------------------------------------------
@@ -57,18 +56,17 @@ def imfillhole_v2(img):
     img_fh = img | img_flood_fill_inv                       # La salida es un OR entre la imagen original y los huecos rellenos.
     return img_fh 
 
-img_fh_v2 = imfillhole_v2(img_close)
-plt.imshow(img_fh_v2, cmap = "gray"), plt.show(block=False)
-
+img_fh = imfillhole_v2(img_close)
+plt.imshow(img_fh, cmap = "gray"), plt.show(block=False)
 
 
 #Componentes conectadas para ver areas
-num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_fh_v2, 4, cv2.CV_32S)
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_fh, 4, cv2.CV_32S)
 
 
 # --- Defino parametros para la clasificación -------------------------------------------
-RHO_TH = 0.8    # Factor de forma (rho), observamos si es circulo o no
-AREA_TH = 5000   # Umbral de area
+RHO_TH = 0.8	# Factor de forma (rho), si es circulo el valor es mayor a 0.8
+AREA_TH = 5000   # Umbral de area para descartar los labels que no sean figuras
 aux = np.zeros_like(labels)
 labeled_image = cv2.merge([aux, aux, aux])
 
@@ -96,31 +94,16 @@ for i in range(1, num_labels):
 
     # --- Clasifico -----------------------------------------------------------
     if rho > RHO_TH:
-        labeled_image[obj == 1, 0] = 255
+        labeled_image[obj == 1, 2] = 255
     else:
         labeled_image[obj == 1, 1] = 255
     
 plt.figure(); plt.imshow(labeled_image); plt.show(block=False)
 
+#_____________________________________________________________EJERCICIO 2________________________________________________________________________________________________
 #________________________________________________________________________________________________________________________________________________________________________
-# Crear una nueva imagen en negro
-dados_image = np.zeros_like(labeled_image)
-
-# Copiar los píxeles verdes a la nueva imagen
-dados_image[:, :, 1] = labeled_image[:, :, 1]
-
-# Mostrar la nueva imagen
-plt.figure()
-plt.imshow(dados_image)
-plt.show(block = False)
-
-# ________________________________________________________________________________________________________________________________________________________________________
-
-monedas_image = np.zeros_like(labeled_image)
-monedas_image[:, :, 0] = labeled_image[:, :, 0]
-
 # Convertir a profundidad de 8 bits por canal
-monedas_8u = monedas_image.astype(np.uint8)
+monedas_8u = labeled_image[:, :, 2].astype(np.uint8)
 
 # Convertir a escala de grises
 monedas_gray = cv2.cvtColor(monedas_8u, cv2.COLOR_BGR2GRAY)
@@ -141,7 +124,7 @@ area_rangos = {
 
 # Crear una lista para almacenar el tipo de cada moneda
 tipos_monedas = []
-
+cant_monedas = {"10 centavos": 0,"50 centavos": 0, "1 peso": 0}
 # Clasificar cada moneda según su área
 for i in range(1, stats.shape[0]):
     area = stats[i, cv2.CC_STAT_AREA]
@@ -149,10 +132,14 @@ for i in range(1, stats.shape[0]):
     # Comparar el área con los rangos definidos
     if area_rangos['10_cent'][0] <= area <= area_rangos['10_cent'][1]:
         tipo = '10 CENT'
+        cant_monedas["10 centavos"] +=1
+
     elif area_rangos['1_peso'][0] <= area <= area_rangos['1_peso'][1]:
         tipo = '1 PESO'
+        cant_monedas["1 peso"] +=1
     elif area_rangos['50_cent'][0] <= area <= area_rangos['50_cent'][1]:
         tipo = '50 CENT'
+        cant_monedas["50 centavos"] +=1
     else:
         tipo = 'No Clasificado'
     
@@ -171,50 +158,68 @@ for tipo, i in tipos_monedas:
     centro = (int(centroids[i, 0]), int(centroids[i, 1]))
     cv2.putText(etiquetas_image, tipo, centro, font, escala, color, 2, cv2.LINE_AA)
 
+# Agregar texto con la cantidad de monedas por tipo
+posicion_y = 2300
+for tipo, cantidad in cant_monedas.items():
+    texto_cantidad = f'{tipo}: {cantidad} monedas'
+    cv2.putText(etiquetas_image, texto_cantidad, (30, posicion_y), font, 3, color, 2, cv2.LINE_AA)
+    posicion_y += 90  # Ajusta el espacio vertical entre líneas según tus preferencias
+
+#Agregar borde a la moneda
+# Encontrar contornos en la imagen después de la morfología
+contours, _ = cv2.findContours(monedas_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Iterar sobre los contornos y dibujar etiquetas de color
+for contour in contours:
+    area = cv2.contourArea(contour)
+    cv2.drawContours(etiquetas_image, [contour], 0, (0, 0, 255), 2)  # Dibujar contorno en rojo
+   
+
 # Mostrar la imagen con texto
-plt.figure()
-plt.imshow(etiquetas_image)
-plt.show(block=False)
+plt.imshow(etiquetas_image), plt.show(block=False)
+
+#_____________________________________________________________EJERCICIO 3________________________________________________________________________________________________
+#________________________________________________________________________________________________________________________________________________________________________
+# Encontrar las coordenadas de los píxeles verdes en la imagen con dados
+coordenadas_verdes = cv2.findNonZero(labeled_image[:, :, 1])
+
+# Crear una máscara en blanco del mismo tamaño que la imagen filtrada en gray scale
+img_dados = np.zeros_like(img_filtered, dtype = np.uint8)
+
+# Dibujar regiones segmentadas en la máscara usando las coordenadas verdes
+for coord in coordenadas_verdes:
+    x, y = coord[0]
+    img_dados[y, x] = img_filtered[y, x]
+
+# Mostrar la máscara con las regiones segmentadas
+plt.imshow(img_dados, cmap = "gray"), plt.show(block=False)
+
+
+#Canny
+img_canny_dados = cv2.Canny(img_dados, 35, 110)
+plt.imshow(img_canny_dados, cmap="gray"), plt.show(block = False)
+
+#Dilato
+# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
+# img_dilated = cv2.dilate(img_canny_CV2, kernel, iterations=1)
+img_dilated_dado = cv2.dilate(img_canny_dados, cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)), iterations=1)
+plt.imshow(img_dilated_dado, cmap = "gray"), plt.show(block=False)
+
+
+#Obtener estructura
+img_open_dado = cv2.morphologyEx(img_dilated_dado, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+img_close_dado = cv2.morphologyEx(img_open_dado, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+plt.imshow(img_close_dado, cmap="gray")
+plt.show(block= False)
+
+
+
+# ________________________________________________________________________________________________________________________________________________________________________
 
 
 
 
 
-
-
-
-
-# Crear una nueva imagen para agregar etiquetas
-labeled_image_with_labels = np.copy(img_color)
-
-# Definir las posiciones para colocar las etiquetas (puedes ajustar estas posiciones según tus necesidades)
-text_positions = {
-    'Tipo1': (10, 30),
-    'Tipo2': (10, 60),
-    'Tipo3': (10, 90),
-    'No Clasificado': (10, 120)
-}
-
-# Agregar etiquetas a la nueva imagen
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 0.5
-font_thickness = 1
-
-for i in range(1, stats.shape[0]):
-    tipo = stats[i, -1]  # La última columna tiene el tipo de moneda
-    text_position = text_positions.get(tipo, (10, 150))  # Posición predeterminada si no se encuentra el tipo
-    
-    # Obtener la posición del centroide del componente conectado
-    centroid_x = int(stats[i, cv2.CC_STAT_LEFT] + stats[i, cv2.CC_STAT_WIDTH] / 2)
-    centroid_y = int(stats[i, cv2.CC_STAT_TOP] + stats[i, cv2.CC_STAT_HEIGHT] / 2)
-    
-    # Escribir la etiqueta en la nueva imagen
-    cv2.putText(labeled_image_with_labels, tipo, (centroid_x - 30, centroid_y), font, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
-
-# Mostrar la nueva imagen con etiquetas
-cv2.imshow('Labeled Image with Labels', labeled_image_with_labels)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
 
@@ -243,21 +248,6 @@ plt.title('Objetos detectados')
 plt.show()
 
 
-#Opcion 2
-# se = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
-# f_mg = cv2.morphologyEx(Fd, cv2.MORPH_GRADIENT, se)
-# plt.imshow(f_mg, cmap= "gray"), plt.show(block = False)
-
-
-# _, img_binary = cv2.threshold(img_filtered1_gray, 42, 255, cv2.THRESH_BINARY)
-# plt.imshow(img_binary, cmap="gray"), plt.show(block = False)
-
-#Encontrar componentes conectadas
-num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(fop_cls, 4, cv2.CV_32S)
-
-im_color = cv2.applyColorMap(np.uint8(255/num_labels*labels), cv2.COLORMAP_INFERNO)
-
-plt.imshow(im_color); plt.show(block=False)
 # escala de grises
 # filtro pasa bajos
 # canny
@@ -268,23 +258,3 @@ plt.imshow(im_color); plt.show(block=False)
 #Los dados los pego en otra, elimino el fondo
 #Relleno los circulos, uso formula para eliminar los que no sean circulos
 # pego la mascara de color sobre la imagen original para que quede mas lindo
-
-
-
-
-
-
-
-
-# Filtrado
-ddepth = cv2.CV_64F  # Formato salida
-grad_x = cv2.Sobel(img_filtered, ddepth, 1, 0, ksize=3) # https://docs.opencv.org/3.4/d4/d86/group__imgproc__filter.html#gacea54f142e81b6758cb6f375ce782c8d
-grad_y = cv2.Sobel(img_filtered, ddepth, 0, 1, ksize=3) # Tutorial: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
-
-grad = np.sqrt(grad_x**2 + grad_y**2)
-np.unique(grad)
-grad_n = cv2.convertScaleAbs(grad)
-np.unique(grad_n)
-
-
-plt.imshow(grad_n, cmap= "gray"); plt.show(block=False)
