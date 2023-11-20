c62c988e6a0dca698b0051eb019a541c96dbb4c7
Patricio <patriciovercesi@hotmail.com>
2023-11-20 09:16:46 -0300

    Ej2

--- ej2.py
+++ ej2.py
@@ -1,37 +1,125 @@
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-def detect_pattent(img):
-    # Load the image
-    img = cv2.imread(img)
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Defininimos función para mostrar imágenes
+def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=True, ticks=False):
+    if new_fig:
+        plt.figure()
+    if color_img:
+        plt.imshow(img)
+    else:
+        plt.imshow(img, cmap='gray')
+    plt.title(title)
+    if not ticks:
+        plt.xticks([]), plt.yticks([])
+    if colorbar:
+        plt.colorbar()
+    if new_fig:        
+        plt.show(block=blocking)
 
-    thresh = cv2.threshold(gray, 100, 254, cv2.THRESH_BINARY_INV)[1]
-    B = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    Aop = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, B)
-    # countors = cv2.findContours(Aop, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
-    connected_components = cv2.connectedComponentsWithStats(Aop, 4, cv2.CV_32S)
-    inverted = cv2.bitwise_not(connected_components[1])
-    plt.imshow(inverted, cmap='gray')
+def detect_patent(filename):
+    # Read the image
+    image = cv2.imread(filename)
+
+    # Convert the image from BGR to HSV
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define lower and upper bounds for white color in HSV format
+    lower_white = np.array([0, 0, 120], dtype=np.uint8)
+    upper_white = np.array([180, 75, 255], dtype=np.uint8)
+    # Create a mask to identify white pixels within the specified HSV range
+    white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+    # Bitwise AND operation to extract white pixels
+    whitish_pixels = cv2.bitwise_and(image, image, mask=white_mask)
+
+    # Convert the Value channel to grayscale
+    gray = whitish_pixels[:, :, 2]
     
-    license_ratio = 0.5
-    min_w = 0
-    max_w = 10
-    min_h = 0
-    max_h = 20
-    candidates = []
-    for cnt in connected_components[1]:
-        x,y,w,h = cv2.boundingRect(cnt)
-        ratio = float(w)/h
-        if (np.isclose(ratio, license_ratio, atol=0.7)):
-            if max_w > w >= min_w and max_h > h >= min_h:
-                candidates.append(cnt)
+    # Laplacian of Gaussian
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    LoG = cv2.Laplacian(blur, cv2.CV_64F, ksize=3)
+    LoG_abs = cv2.convertScaleAbs(LoG)   # Pasamos a 8 bit
+    LoG_abs_th = LoG_abs > LoG_abs.max()*0.45
+    filtered = LoG_abs_th.astype(np.uint8) * 255
+    
+    # Morphological Opening & Closing
+    B = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    filtered = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, B)
+    filtered = cv2.morphologyEx(filtered, cv2.MORPH_CLOSE, B)
 
-    plt.imshow(candidates[0], cmap='gray')
+    num_labels, labels, stats, centroids  = cv2.connectedComponentsWithStats(filtered, 4, cv2.CV_32S)
 
-    return thresh
+    possible_patents = []
+    for st in stats:
+        x, y, w, h, area = st
+        ratio = w / h
+        if  (1 < ratio < 3) and (65 < w < 110) and (20 < x < 620) and (20 < y < 460):
 
-img = 'Patentes/img01.png'
-img = detect_pattent(img)
+            highlighted = cv2.rectangle(image.copy(), (x, y), (x + w, y + h), color=(0, 255, 0), thickness=1)
+            possible_patents.append(image[y-5:y+h+5, x-5:x+w+5])
+
+    #cv2.imshow(filename, highlighted)
+    #cv2.waitKey(0)
+    return possible_patents
+
+
+def detect_characters(patent, filename):
+        # Convert the image from BGR to HSV
+        hsv_image = cv2.cvtColor(patent, cv2.COLOR_RGB2HSV)
+
+        # Define lower and upper bounds for white color in HSV format
+        lower_white = np.array([0, 0, 120], dtype=np.uint8)
+        upper_white = np.array([180, 75, 255], dtype=np.uint8)
+        # Create a mask to identify white pixels within the specified HSV range
+        white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+        # Bitwise AND operation to extract white pixels
+        whitish_pixels = cv2.bitwise_and(patent, patent, mask=white_mask)
+
+        # Convert the Value channel to grayscale
+        gray = whitish_pixels[:, :, 2]
+        
+        num_labels, labels, stats, centroids  = cv2.connectedComponentsWithStats(gray, 4, cv2.CV_32S)
+
+        characters = []
+        for st in stats:
+            x, y, w, h, area = st
+            ratio = w / h
+            if (0.3 < ratio < 1) and (10 < h):
+                highlighted = cv2.rectangle(patent.copy(), (x, y), (x + w, y + h), color=(0, 255, 0), thickness=1)
+                characters.append(patent[y:y+h, x:x+w])
+
+        #cv2.imshow(filename, highlighted)
+        #cv2.waitKey(0)
+        return characters
+
+
+
+def show_all_characters():
+    for i in range (1,13):
+        filename = 'Patentes/img'+("0"+str(i) if i<=9 else str(i))+'.png'
+        possible_patents = detect_patent(filename)
+        if possible_patents == []:
+            print('No se detectó posible patente en ' + filename)
+            continue
+        six_char = False
+        for patent in possible_patents:
+            characters = detect_characters(patent, filename)
+            if len(characters) == 6:
+                six_char = True
+                break
+        if six_char:
+            imshow(patent)
+
+            fig, axs = plt.subplots(1, 6, figsize=(4, 1))
+            for j in range(len(characters)):
+                axs[j].axis('off')
+                axs[j].imshow(characters[j])
+            plt.suptitle(filename, fontsize=16)
+            plt.tight_layout()
+            plt.show()
+        else:
+            print('No se detectaron 6 caracteres en ' + filename)
+
+
+show_all_characters()
